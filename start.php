02d30746e7fa261f2360c11f0048295f14dbe51b
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-02-15 19:29:19 +0100

    substr part added to the code

--- start.php
+++ start.php
@@ -3,4 +3,18 @@ echo'<h1>hello world</h1>';
 echo'<p>we are the world</p>'.'<br>';
 $name = 'cino'.'<br>';
 echo $name;
+
+$chaine = "ceci est une chaine de caractere";
+// echo $chaine . '<br>';
+//afficher un caractere de la chaine
+// echo $chaine[3].'<br>';
+//modifier un caractere de la chaine
+// $chaine[0]="F".'<br>';
+// echo $chaine.'<br>';
+// var_dump($chaine).'<br>';
+// echo substr($chaine,0,4) .'br>';
+// echo substr($chaine,-10) .'br>'
+//remplacer une partie de la chaine du caractere
+$chaine=str_replace('ceci','cela',$chaine).'br>';
+var_dump(str)
 ?>
